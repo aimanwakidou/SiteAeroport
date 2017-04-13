36aa7cec10b4a7f4a6676f01e01928d0a1a02614
AïmanWakidou <aimanwakidou@gmail.com>
2017-04-14 00:35:54 +0200

    MAJ AfficheMenuMobile

--- js/afficheMenuMobile.js
+++ js/afficheMenuMobile.js
@@ -2,14 +2,10 @@
 function AfficheMenuMobile(){
 	var rotateElem = $("#rotateElem");
 	if(rotateElem.attr('class') === "fa fa-plane clicked"){
-		rotateElem.css({WebkitTransform:'rotate('+0+'deg)'});
-		rotateElem.css({'-moz-transform':'rotate('+0+'deg)'});
 		rotateElem.css('transform','rotate('+0+'deg)');
 		rotateElem.removeClass("clicked");
 	}
 	else{
-		rotateElem.css({WebkitTransform:'rotate('+90+'deg)'});
-		rotateElem.css({'-moz-transform':'rotate('+90+'deg)'});
 		rotateElem.css('transform','rotate('+90+'deg)');
 		rotateElem.addClass("clicked");
 	}
