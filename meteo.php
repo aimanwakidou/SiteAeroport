22c941215f802ac88af8dd8f00af069b06880e2c
Aiman Wakidou <Wakidou@vps401843.ovh.net>
2017-04-23 13:19:02 +0200

    MeteoPHP

---
+++ meteo.php
@@ -0,0 +1,30 @@
+<?php
+header( 'Content-Type: application/json' );
+ini_set("allow_url_fopen", 1);
+$iles = array('Moutsamoudou','Mamoudzou','Moheli','Moroni');
+
+foreach($iles as $ile) {
+    $json = file_get_contents('http://api.wunderground.com/api/5fe80449e735d30f/conditions/q/COMOROS/'.$ile.'.json');
+    
+    switch ($ile) {
+    case "Moutsamoudou":
+       $fp = fopen('Anjouan.json', 'w');
+        break;
+    case "Mamoudzou":
+        $fp = fopen('Mayotte.json', 'w');
+        break;
+    default:
+       $fp = fopen($ile.'.json', 'w');
+        break;
+    }
+    
+    $obj = json_decode($json);
+    fwrite($fp, json_encode($obj));
+    fclose($fp);
+}
+
+
+
+
+
+?>
--- meteo.py
+++ meteo.py
@@ -1,17 +1,18 @@
 #-*- encoding:utf-8 -*-
 import requests
 dataJSONIles = ('Moutsamoudou','Mamoudzou','Moheli','Moroni')
+executionDirectory = "/home/Wakidou/SiteAeroport/"
 for ile in dataJSONIles :
     r = requests.get('http://api.wunderground.com/api/5fe80449e735d30f/conditions/q/COMOROS/'+ile+'.json')
     print "code de renvoi HTTP pour {1} : {0}".format(r.status_code,ile)
 
     file = None
     if ile == 'Moutsamoudou' :
-        file = open("Anjouan.json","w")
+        file = open(executionDirectory+"Anjouan.json","w")
     elif ile == 'Mamoudzou' :
-        file = open("Mayotte.json","w")
+        file = open(executionDirectory+"Mayotte.json","w")
     else :
-        file = open(ile+".json","w")
+        file = open(executionDirectory+ile+".json","w")
         
     file.writelines(r.text)
     file.close()
