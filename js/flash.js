0ed74024ad39fad9aef627a09b8772953b3784fa
Aiman Wakidou <Wakidou@vps401843.ovh.net>
2017-05-05 19:14:38 +0200

    MAJ : Modification API

--- js/flash.js
+++ js/flash.js
@@ -2,15 +2,28 @@ $('#zoneFlashAlert').submit(function(event) {
 	event.preventDefault();
 
 	var url = "https://5.196.225.5/Flash/sendFlash";
-	$.post(url,
+	var tabVols = [];
+	 $('#zoneFlashAlert #BoxFlashAlert input:visible[type=text]').each(function () {
+        tabVols.push( $(this).val());
+        
+        });
+
+	 
+	 
+ 	$.post(url,
 	{
+		vols:tabVols,
 		num_vol:$("#num_vol_flash").val(),
 		email:$("#email2").val(),
+		nom:$("#nomFlash").val(),
+		prenom:$("#prénomFlash").val(),
 		tel:$("#tel2").val()
 	},
 
 	function(data,status){
 		console.log(status);
 	});
+	 
+	
 
 });
