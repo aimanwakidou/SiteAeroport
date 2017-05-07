0d336b42794ca71cdaa954922be0f036db3359b8
Aiman Wakidou <Wakidou@vps401843.ovh.net>
2017-05-07 18:11:49 +0200

    Fusion API

--- js/flash.js
+++ js/flash.js
@@ -2,7 +2,7 @@ $('#zoneFlashAlert').submit(function (event) {
     event.preventDefault();
     var first_vol = $("#first_vol input");
     if (first_vol.val().length) {
-        var url = "https://5.196.225.5/Flash/sendFlash";
+        var url = "https://5.196.225.5/api/Flash/";
         var tabVols = [];
 
         $('#BoxFlashAlert .search-box-inner input').each(function () {
