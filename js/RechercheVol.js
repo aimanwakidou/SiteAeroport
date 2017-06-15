35a429baf619da0a16ae8ac474b02e57f4a0cf61
aïman.wakidou <aman.wakidou@VN-PC50>
2017-06-15 18:38:27 +0200

    Récupération vol javascript

--- js/RechercheVol.js
+++ js/RechercheVol.js
@@ -1,9 +1,20 @@
 // JavaScript source code
 $("#zoneRecherche").submit(function (event) {
     event.preventDefault();
-    var provenance = $("#Provenance").val();
-    var destination = $("#Destination").val();
+    var provenance = $("#Provenance");
+    var destination = $("#Destination");
     var dateVol = $('input[name="dateVol"]').val();
+    dateVol = dateVol.replace(/[/]/g, '-');
 
-    console.log("Date du vol : "+dateVol);
+    if (provenance.parent().hasClass('success') && destination.parent().hasClass('success') && dateVol.length != 0) {
+        var url = "https://5.196.225.5/api/RechercheVols/" + provenance.val() + "/" + destination.val() + "/" + dateVol;
+        $.getJSON(url, {})
+
+            .done(function (data) {
+                Object.keys(data).forEach(function (key) {
+                    console.log(data[key]);
+                });
+            });
+    }
+
 });
