c16ce3fd44bb6d1249255b13cf771118041beb5e
aïman.wakidou <aman.wakidou@VN-PC50>
2017-06-22 15:42:00 +0200

    Ajout TimePicker + Check HTML 5

--- js/DatePicker.js
+++ js/DatePicker.js
@@ -1,7 +1,4 @@
-/*No Conflict Mode*/
-/*var datepicker = $.fn.datepicker.noConflict();
-$.fn.bootstrapDP = datepicker;*/
-
+/*Paramètre DatePicker*/
 $(".DatePickerInput input,.JourInput input").datepicker({
     format: "dd/mm/yyyy",
     startDate: "new Date()",
---
+++ js/TimePicker.js
@@ -0,0 +1,10 @@
+/*Paramétrage TimePicker*/
+$(".TimePickerInput input").timepicker({
+    minuteStep:1,
+    template:false,
+    showMeridian: false,
+    icons: {
+        up: 'glyphicon glyphicon-chevron-up',
+        down: 'glyphicon glyphicon-chevron-down'
+    }
+});
