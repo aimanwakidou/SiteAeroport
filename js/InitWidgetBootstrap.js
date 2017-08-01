1d1a480363e0f92523fced020bb31acd94f1b3d1
aïman.wakidou <aman.wakidou@VN-PC50>
2017-08-01 18:21:50 +0200

    Integration datetimepicker

--- js/InitWidgetBootstrap.js
+++ js/InitWidgetBootstrap.js
@@ -8,16 +8,6 @@ $(".DatePickerInput input,.JourInput input").datepicker({
     autoclose:true
 });
 
-/*Paramétrage TimePicker*/
-$(".TimePickerInput input").timepicker({
-    minuteStep: 1,
-    template: false,
-    showMeridian: false,
-    icons: {
-        up: 'glyphicon glyphicon-chevron-up',
-        down: 'glyphicon glyphicon-chevron-down'
-    }
-});
 
 /*Paramètrage selectpicker*/
 $(".selectpicker").selectpicker({
@@ -38,6 +28,12 @@ $(document).ready(function () {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
         $('.selectpicker').selectpicker('mobile');
     }
+
+    /*Paramétrage TimePicker*/
+    $(".TimePickerInput").datetimepicker({
+        pickDate: false,
+        pickSeconds: false
+    });
 }); 
 
 
